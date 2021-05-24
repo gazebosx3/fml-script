1ecb8cb3e1efa4e39e62ea6f800d5b48a8fe735f
Sam Glass <samuelglass@gmail.com>
2021-05-23 21:32:45 -0400

    Generate picks

--- index.js
+++ index.js
@@ -1,36 +1,67 @@
-const users = ['sam', 'josh', 'evan', 'gillian']
+const users = ['sam', 'josh', 'evan', 'gillian', 'natalie', 'jake', 'adam', 'tom', 'emily']
 
-const songs = {
-    sam: ['sam song 1', 'sam song 2'],
-    josh: ['josh song 1', 'josh song 2'],
-    evan: ['evan song 1', 'evan song 2'],
-    gillian: ['gillian song 1', 'gillian song 2'],
+let weekNumber = 8
+
+function generateSongs() {
+    const res = {}
+
+    users.forEach(user => {
+        res[user] = []
+        for (let i = 0; i < weekNumber; i++) {
+            res[user].push(`${user} song ${i+1}`)
+        }
+    })
+
+    return res
 }
 
-const picks = {
-    sam: {
-        weekOne: 'josh song 1',
-        weekTwo: 'evan song 2'
-    },
-    josh: {
-        weekOne: 'sam song 1',
-        weekTwo: 'sam song 2'
-    },
-    evan: {
-        weekOne: 'sam song 1',
-        weekTwo: 'gillian song 2'
-    },
-    gillian: {
-        weekOne: 'josh song 1',
-        weekTwo: 'evan song 2'
+const songs = generateSongs()
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+  }
+
+function generatePicks() {
+    const res = {}
+
+    for (let user in songs) {
+        res[user] = {}
+        for (let i = 0; i < weekNumber; i++) {
+            const randUser1 = users[getRandomIntInclusive(0, users.length-1)]
+            let randUser2 = users[getRandomIntInclusive(0, users.length-1)]
+            if (randUser1 === randUser2) {
+                randUser2 = users[getRandomIntInclusive(0, users.length-1)]
+            }
+            let randUser3 = users[getRandomIntInclusive(0, users.length-1)]
+            if (randUser3 === randUser2 || randUser3 === randUser1) {
+                randUser3 = users[getRandomIntInclusive(0, users.length-1)]
+            }
+
+            const m = `week${i+1}`
+            res[user][m] = []
+            res[user][m].push(songs[randUser1][i])
+            res[user][m].push(songs[randUser2][i])
+            res[user][m].push(songs[randUser3][i])
+
+
+
+        }
     }
+
+    return res
 }
+
+const picks = generatePicks()
+
+console.log('picks is: ', picks)
+
 
 function getUsersVotesForOtherUsers(user) {
 
     const userPicks = picks[user]
 
-    // user: numvotes
     const otherUserVoteFreq = {}
 
     for (const week in userPicks) {
@@ -105,3 +136,5 @@ function whoLovesYaBaby(primaryUser) {
     return finalString
 
 }
+
+console.log(whoLovesYaBaby('gillian'))
